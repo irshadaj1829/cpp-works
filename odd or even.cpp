027cab56b59a92874754c8b06ffd1675d15b5578
MUHAMMED IRSHAD A J <55504536+irshadaj1829@users.noreply.github.com>
2020-05-10 20:49:09 +0530

    check odd or even multiple times

---
+++ odd or even.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,m;
+	int n,i;
+	cout<<"enter no of loops\n";
+	cin>>n;
+	
+	for(i=1;i<=n;i++)
+	
+	{
+	
+			cout<<"\nenter a number\n";
+	cin>>a;
+	m=a%2;
+	
+	
+	{
+	if(m==0)
+	{
+		cout<<"it is an even number\n\n";
+	}
+	if(m!=0)
+	{
+		cout<<"it is an odd number\n\n";
+	}
+	
+	}
+	}
+}
